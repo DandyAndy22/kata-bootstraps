8fa58a84da47176ba3fbda87ea8aa55fbace3f18
dandyandy22 <andrasm96@gmail.com>
2022-05-16 07:38:16 -0700

    Scaffold out preliminary tests for kata

---
+++ ruby/rspec/spec/test.rb
@@ -0,0 +1,39 @@
+describe 'christmas light kata' do
+  describe 'santas instructions' do
+    it 'turns on lights 887,9 through 959,629' do
+      skip
+    end
+
+    it 'turns on lights 454,398 through 844,448' do
+      skip
+    end
+
+    it 'turns off lights 539,243 through 559,965' do
+      skip
+    end
+
+    it 'turns off lights 370,819 through 676,868' do
+      skip
+    end
+
+    it 'turns off lights 145,40 through 370,997' do
+      skip
+    end
+
+    it 'turns off lights 301,3 through 808,453' do
+      skip
+    end
+
+    it 'turns on lights 351,678 through 951,908' do
+      skip
+    end
+
+    it 'toggle lights 720,196 through 897,994' do
+      skip
+    end
+
+    it 'toggle lights 831,394 through 904,860' do
+      skip
+    end
+  end
+end
